f802b407b4c54377348e79ab2cb83c193c530488
Константин Сараев <Badmantegrow1@gmail.com>
2022-09-30 20:01:15 +0300

    Yatube, 2 спринт, версия №1

---
+++ yatube/posts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField('Название', max_length=200)
+    slug = models.SlugField('Слаг', unique=True)
+    description = models.TextField('Описание')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField('Текст поста')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               verbose_name='Автор',
+                               related_name='posts_author')
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              verbose_name='Группа',
+                              related_name="posts_group", blank=True,
+                              null=True)
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.text
